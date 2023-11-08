e6d9fe611a5288ffe8286511fdcf7566d9350231
anywxn <54641744+anywxn@users.noreply.github.com>
2023-11-09 00:05:57 +0500

    added sonic sprites animation
    added sonic sprites animation

--- Player/player.gd
+++ Player/player.gd
@@ -17,15 +17,19 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	if Input.is_action_just_pressed("attack") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		anim.play("Jump")
+
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("Jump")
 
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.y == 0:
